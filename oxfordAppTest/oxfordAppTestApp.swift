b190c749817f8aa3287300f6881b01a25e3bc3e3
Melih Şişkular <feis_melih@hotmail.com>
2025-01-31 01:02:46 +0700

    Initial Commit

---
+++ oxfordAppTest/oxfordAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  oxfordAppTestApp.swift
+//  oxfordAppTest
+//
+//  Created by Melih Şişkular on 31.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct oxfordAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
